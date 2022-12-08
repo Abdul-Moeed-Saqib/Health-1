81a970277babec8842acb19f253995390ccebe6c
Alejandro ZZ <ale0720199@gmail.com>
2022-12-08 13:06:05 -0500

    emergency Alert

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -1,6 +1,6 @@
 const User = require("../models/user");
 const MotivationalTip = require("../models/motivationalTip");
-const EmergencyAlertType = require("../models/emergencyAlert");
+const EmergencyAlert = require("../models/emergencyAlert");
 const VitalSign = require("../models/vitalSign");
 
 const bcrypt = require("bcrypt");
@@ -73,53 +73,72 @@ const VitalSignType = new GraphQLObjectType({
   }),
 });
 
-
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
     user: {
       type: UserType,
-      args: { id: { name: 'email', type: GraphQLString}},
+      args: { id: { name: "email", type: GraphQLString } },
       resolve(parent, args) {
-          const email = args.id;
-          const user = User.findOne({email});
-          return user;
-      }
+        const email = args.id;
+        const user = User.findOne({ email });
+        return user;
+      },
     },
     motiavtionalTip: {
       type: MotivationalTipType,
       resolve(parent, args) {
-        const rand = Math.floor(Math.random() * MotivationalTip.count())
+        const rand = Math.floor(Math.random() * MotivationalTip.count());
         const motiavTip = MotivationalTip.findOne().skip(rand);
         return motiavTip;
-      }
-    }
-  }
+      },
+    },
+  },
 });
 
 const mutation = new GraphQLObjectType({
-  name: 'Mutation',
+  name: "Mutation",
   fields: {
     addMotiavtionalTip: {
       type: MotivationalTipType,
       args: {
-        description: { type: GraphQLNonNull(GraphQLString)},
-        nurseId: { type: GraphQLNonNull(GraphQLString)}
+        description: { type: GraphQLNonNull(GraphQLString) },
+        nurseId: { type: GraphQLNonNull(GraphQLString) },
       },
-      resolve: async function(parent, args) {
+      resolve: async function (parent, args) {
         try {
-          const motiavtional = MotivationalTip.create({description: args.description, nurse: args.nurseId});
+          const motiavtional = MotivationalTip.create({
+            description: args.description,
+            nurse: args.nurseId,
+          });
           return motiavtional;
         } catch (error) {
           throw Error(error.message);
         }
-      }
-    }
-  }
-})
+      },
+    },
+    addEmergencyAlert: {
+      type: MotivationalTipType,
+      args: {
+        content: { type: GraphQLNonNull(GraphQLString) },
+        patientId: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async function (parent, args) {
+        try {
+          const emerAlert = EmergencyAlert.create({
+            content: args.content,
+            patient: args.patientId,
+          });
+          return emerAlert;
+        } catch (error) {
+          throw Error(error.message);
+        }
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  mutation
-})
+  mutation,
+});
