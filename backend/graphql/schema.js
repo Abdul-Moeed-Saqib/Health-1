4b8f4e9dfc6594a5f16a98828e30aa483b2b550e
Alejandro ZZ <ale0720199@gmail.com>
2022-12-08 20:44:27 -0500

    mutation for emer & vital

--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -1,7 +1,7 @@
 const MotivationalTip = require("../models/motivationalTip");
 const EmergencyAlert = require("../models/emergencyAlert");
 const VitalSign = require("../models/vitalSign");
-const { userQuery } = require('./userSchema')
+const { userQuery } = require("./userSchema");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -17,7 +17,7 @@ const {
   GraphQLID,
 } = require("graphql");
 
-const { UserType } = require('./userSchema')
+const { UserType } = require("./userSchema");
 // const UserType = new GraphQLObjectType({
 //   name: "User",
 //   fields: () => ({
@@ -73,8 +73,6 @@ const VitalSignType = new GraphQLObjectType({
   }),
 });
 
-
-
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
@@ -82,36 +80,111 @@ const RootQuery = new GraphQLObjectType({
     motiavtionalTip: {
       type: MotivationalTipType,
       resolve: async (parent, args) => {
-        const rand = Math.floor(Math.random() * MotivationalTip.count())
+        const rand = Math.floor(Math.random() * MotivationalTip.count());
         const motiavTip = await MotivationalTip.findOne().skip(rand);
         return motiavTip;
-      }
-    }
-  }
+      },
+    },
+  },
 });
 
 const mutation = new GraphQLObjectType({
-  name: 'Mutation',
+  name: "Mutation",
   fields: {
+    //MotiavtionalTip mutation
     addMotiavtionalTip: {
       type: MotivationalTipType,
       args: {
         description: { type: GraphQLNonNull(GraphQLString) },
-        nurseId: { type: GraphQLNonNull(GraphQLString) }
+        nurseId: { type: GraphQLNonNull(GraphQLString) },
       },
       resolve: async function (parent, args) {
         try {
-          const motiavtional = await MotivationalTip.create({ description: args.description, nurse: args.nurseId });
+          const motiavtional = await MotivationalTip.create({
+            description: args.description,
+            nurse: args.nurseId,
+          });
           return motiavtional;
         } catch (error) {
           throw Error(error.message);
         }
-      }
-    }
-  }
-})
+      },
+    },
+    deleteMotiavtionalTip: {
+      type: MotivationalTipType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return MotivationalTip.findByIdAndRemove(args.id);
+      },
+    },
+    //EmergencyAlert mutation
+    addEmergencyAlert: {
+      type: EmergencyAlertType,
+      args: {
+        content: { type: GraphQLNonNull(GraphQLString) },
+        patientId: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async function (parent, args) {
+        try {
+          const emerAlert = await EmergencyAlert.create({
+            constent: args.constent,
+            parent: args.patientId,
+          });
+          return emerAlert;
+        } catch (error) {
+          throw Error(error.message);
+        }
+      },
+    },
+    deleteEmergencyAlert: {
+      type: EmergencyAlertType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return EmergencyAlert.findByIdAndRemove(args.id);
+      },
+    },
+    //VitalSign mutation
+    addVitalSign: {
+      type: VitalSignType,
+      args: {
+        bodyTem: { type: GraphQLNonNull(GraphQLFloat) },
+        heartRate: { type: GraphQLNonNull(GraphQLFloat) },
+        bloodPre: { type: GraphQLNonNull(GraphQLFloat) },
+        respiratoryRate: { type: GraphQLNonNull(GraphQLFloat) },
+        patientId: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: async function (parent, args) {
+        try {
+          const vitalSign = await VitalSign.create({
+            bodyTem: args.bodyTem,
+            heartRate: args.heartRate,
+            bloodPre: args.bloodPre,
+            respiratoryRate: args.respiratoryRate,
+            patientId: args.parentId,
+          });
+          return vitalSign;
+        } catch (error) {
+          throw Error(error.message);
+        }
+      },
+    },
+    deleteVitalSign: {
+      type: VitalSignType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(paretn, args) {
+        return VitalSign.findByIdAndRemove(args.id);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
-  mutation
-})
+  mutation,
+});
--- backend/graphql/userSchema.js
+++ backend/graphql/userSchema.js
@@ -3,44 +3,76 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-
 // define type
 const UserType = new GraphQLObjectType({
-    name: "User",
-    fields: () => ({
-        email: { type: GraphQLString },
-        password: { type: GraphQLString },
-        firstName: { type: GraphQLString },
-        lastName: { type: GraphQLString },
-        address: { type: GraphQLString },
-        city: { type: GraphQLString },
-        phoneNumber: { type: GraphQLString },
-        email: { type: GraphQLString },
-        role: { type: GraphQLString },
-    }),
+  name: "User",
+  fields: () => ({
+    email: { type: GraphQLString },
+    password: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    lastName: { type: GraphQLString },
+    address: { type: GraphQLString },
+    city: { type: GraphQLString },
+    phoneNumber: { type: GraphQLString },
+    email: { type: GraphQLString },
+    role: { type: GraphQLString },
+  }),
 });
 
-
+//query all the users
 const user = {
-    type: UserType,
-    args: { id: { name: 'email', type: GraphQLString } },
-    resolve: async (parent, args) => {
-        const email = args.id;
-        const user = await User.findOne({ email });
-        return user;
-    }
-}
+  type: UserType,
+  args: { id: { name: "email", type: GraphQLString } },
+  resolve: async (parent, args) => {
+    const email = args.id;
+    const user = await User.findOne({ email });
+    return user;
+  },
+};
+
+//query a list of patients
 const patients = {
-    type: new GraphQLList(UserType),
-    resolve: async (parent, args) => {
-        const patients = await User.find({ role: 'patient' })
-        return patients;
-    }
-}
+  type: new GraphQLList(UserType),
+  resolve: async (parent, args) => {
+    const patients = await User.find({ role: "patient" });
+    return patients;
+  },
+};
+
+//query a single patien
+const patient = {
+  type: UserType,
+  args: { id: { type: GraphQLID } },
+  resolve: async (parent, args) => {
+    const patient = await User.findById(args.id);
+    return patient;
+  },
+};
+//query a list of nurse
+const nurses = {
+  type: new GraphQLList(UserType),
+  resolve: async (parent, args) => {
+    const nurses = await User.find({ role: "nurse" });
+    return nurses;
+  },
+};
+
+//query a single nurse
+const nurse = {
+  type: UserType,
+  args: { id: { type: GraphQLID } },
+  resolve: async (parent, args) => {
+    const nurse = await User.findById(args.id);
+    return nurse;
+  },
+};
 
 const userQuery = {
-    user,
-    patients
-}
+  user,
+  patients,
+  patient,
+  nurses,
+  nurse,
+};
 
-module.exports = { userQuery, UserType }
+module.exports = { userQuery, UserType };
