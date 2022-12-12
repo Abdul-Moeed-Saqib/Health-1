18c736a836122f98db02d4eb6a85725fe47c8b62
Alejandro ZZ <ale0720199@gmail.com>
2022-12-11 19:34:15 -0500

    MemoryGame for patient

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify'
 import { useAuthContext } from './hooks/useAuthContext';
-
+import MemoryGame from './pages/gameForPatient/MemoryGame';
 export default function App() {
   const { user } = useAuthContext();
 
@@ -22,6 +22,7 @@ export default function App() {
           path='*'
           element={!user ? <Auth /> : <Navigate to="/" />}
         />
+        <Route path="/memoryGame" element={<MemoryGame />} />
       </Routes>
       <ToastContainer />
     </Fragment>
